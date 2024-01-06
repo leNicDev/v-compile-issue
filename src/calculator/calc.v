c8cf53fb75c415f4a03698870212a0e5d1653dae
leNicDev <germanelectronix@gmail.com>
2024-01-06 18:59:00 +0100

    initial commit

---
+++ src/calculator/calc.v
@@ -0,0 +1,5 @@
+module calculator
+
+pub fn add(a int, b int) {
+  return a + b
+}
---
+++ src/main.v
@@ -0,0 +1,8 @@
+module main
+
+import calculator
+
+fn main() {
+        result := calculator,add(1, 2)
+	println('1 + 2 = ${result}')
+}
